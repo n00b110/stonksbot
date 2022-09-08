2c656379e5834650ddf893f8d1866397db05a8df
Immanuel Olaoye <olaoye939@gmail.com>
2022-09-07 20:20:27 -0500

    Added stock lookup

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 import discord
+import yfinance as yf
 from discord.ext import commands
+from re import match
 import os
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
 token = os.getenv('TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="&", intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -20,6 +21,13 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
 
+
+@bot.command()
+async def check(ctx, arg):
+    stock_info = yf.Ticker(arg).info
+    stock_price = stock_info["regularMarketPrice"]
+    await ctx.send(stock_price)
+    
 
 bot.run(token)
 
